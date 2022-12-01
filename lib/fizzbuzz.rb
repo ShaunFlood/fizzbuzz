b3cbedfdb8cb1067d65e934240e178845bd29b03
shaun <shaunflood@hotmail.co.uk>
2022-12-01 17:34:20 +0000

    fizzbuzzlib

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,9 @@
-(1..20).each do |n|
-    if n % 3 == 0 && n % 5 == 0
-      puts 'FizzBuzz'
-    elsif n % 3 == 0 
-      puts 'Fizz'
-    elsif n % 5 == 0
-      puts 'Buzz'
+def fizzbuzz(number)
+    if number == 3
+      return "fizz"
+    elsif number == 5
+        return "buzz"
     else
-      puts n 
-    end 
-  end
+        number
+    end
+end
